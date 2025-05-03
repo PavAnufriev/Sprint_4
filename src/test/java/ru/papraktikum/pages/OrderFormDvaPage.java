bee9063e869f57bca65fc21511571a836b811cbe
Pavel Anufriev <anufriev.geo@mail.ru>
2025-05-03 11:49:34 +0200

    Правки после ревью

---
+++ src/test/java/ru/papraktikum/pages/OrderFormDvaPage.java
@@ -0,0 +1,110 @@
+package ru.papraktikum.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+// Временно добавил комментарий для обновления коммита
+
+public class OrderFormDvaPage {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    public OrderFormDvaPage (WebDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+    }
+
+
+    //Локаторы
+    //Локатор для поля Календарь
+    private By fieldKalendar = By.xpath("//input[contains (@placeholder, '* Когда привезти самокат')]");
+    //Локатор для поля Срок аренды
+    private By fieldSrokArendi = By.xpath("//div[text()= '* Срок аренды']");
+    //Локатор для чекбокса Цвет самоката
+    private By fieldCvetSamokataBlack = By.xpath("//input[@id =  'black']");
+    private By fieldCvetSamokataGrey = By.xpath("//input[@id =  'grey']");
+    //Локатор для поля Комментарий для курьера
+    private By fieldKommentarii = By.xpath("//input[contains (@placeholder, 'Комментарий для курьера')]");
+    //Локатор для кнопки Заказать
+    private By buttonZakazat = By.xpath("//button[contains (@class, 'Button_Middle__1CSJM') and text()='Заказать']");
+    //Локатор выпадающего списка Срок аренды
+    private By arrayArenda = By.xpath("//div[text()='двое суток']");
+    //Локатор Всплывающее окно подтверждения
+    private By buttonOknoDa = By.xpath("//button[contains (@class, 'Button_Middle__1CSJM') and text()='Да']");
+    //Локатор Всплывающее окно номер заказа
+    private By windowNomerZakaza = By.xpath("//button[contains(text(), 'Посмотреть статус')]");
+    //Локатор в всплывающем окне Заказ оформлен
+    private By confirmationOrder = By.xpath("//*[text()='Заказ оформлен']");
+
+
+    //Кликаем на поле вносим данные
+    public void clickFieldKalendar(String data){
+        WebElement setFieldKalendar = driver.findElement(fieldKalendar);
+        setFieldKalendar.click();
+        setFieldKalendar.sendKeys(data);
+        setFieldKalendar.sendKeys(Keys.ENTER);
+        }
+
+    public void clickFieldArenda () {
+        WebElement setFieldArenda = driver.findElement(fieldSrokArendi);
+        setFieldArenda.click();
+        wait.until(ExpectedConditions.elementToBeClickable(arrayArenda)).click();
+        //setFieldArenda.sendKeys(Keys.ENTER);
+    }
+
+   public void clickCvetSamokata (String color) {
+       WebElement checkBoxBlack = driver.findElement(fieldCvetSamokataBlack);
+       WebElement checkBoxGrey = driver.findElement(fieldCvetSamokataBlack);
+        if (color.equalsIgnoreCase( "grey")) {
+            checkBoxGrey.click();
+            } else if (color.equalsIgnoreCase("black")){ checkBoxBlack.click();}
+
+   }
+
+    public void clickFieldKomentarii (String komentarii) {
+        WebElement setFieldKomentarii = driver.findElement(fieldKommentarii);
+        setFieldKomentarii.click();
+        setFieldKomentarii.sendKeys(komentarii);
+        setFieldKomentarii.sendKeys(Keys.ENTER);
+    }
+
+    public void clickButtonZakazat (){
+        driver.findElement(buttonZakazat).click();
+    }
+
+    public void clickButtonDa () {
+        WebElement buttonDa = wait.until(ExpectedConditions.elementToBeClickable(buttonOknoDa));
+        buttonDa.click();
+    }
+
+    public void clickWindowNomerZakaza () {
+        WebElement buttonWindow = wait.until(ExpectedConditions.elementToBeClickable(windowNomerZakaza));
+        buttonWindow.click();
+    }
+
+    public String getConfimationText () {
+        WebElement confirmation = wait.until(ExpectedConditions.visibilityOfElementLocated(confirmationOrder));
+        return confirmation.getText();
+    }
+
+    public void orderPageTwo (String data, String color, String komentarii) {
+
+        clickFieldKalendar(data);
+        clickFieldArenda();
+        clickCvetSamokata(color);
+        clickFieldKomentarii(komentarii);
+        clickButtonZakazat();
+        clickButtonDa();
+
+    }
+
+}
+
+
--- src/test/java/ru/papraktikum/pages/StartPage.java
+++ src/test/java/ru/papraktikum/pages/StartPage.java
@@ -1,30 +1,46 @@
 package ru.papraktikum.pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.Arrays;
 import java.util.List;
 
 // Временно добавил комментарий для обновления коммита
 
 
 public class StartPage {
+    public static final String BASE_URL = "https://qa-scooter.praktikum-services.ru";
     private WebDriver driver;
     private WebDriverWait wait;
+
+    public static final List<String> expectedAnswers = Arrays.asList (
+            "Сутки — 400 рублей. Оплата курьеру — наличными или картой.",
+            "Пока что у нас так: один заказ — один самокат. Если хотите покататься с друзьями, можете просто сделать несколько заказов — один за другим.",
+            "Допустим, вы оформляете заказ на 8 мая. Мы привозим самокат 8 мая в течение дня. Отсчёт времени аренды начинается с момента, когда вы оплатите заказ курьеру. Если мы привезли самокат 8 мая в 20:30, суточная аренда закончится 9 мая в 20:30.",
+            "Только начиная с завтрашнего дня. Но скоро станем расторопнее.",
+            "Пока что нет! Но если что-то срочное — всегда можно позвонить в поддержку по красивому номеру 1010.",
+            "Самокат приезжает к вам с полной зарядкой. Этого хватает на восемь суток — даже если будете кататься без передышек и во сне. Зарядка не понадобится.",
+            "Да, пока самокат не привезли. Штрафа не будет, объяснительной записки тоже не попросим. Все же свои.",
+            "Да, обязательно. Всем самокатов! И Москве, и Московской области.");
+
 
 
 
     // Локаторы
+    //Локатор для выпадающего списка с текстом
+    private By listVoprosi = By.xpath("//div[@id='accordion__heading-0']");
     // Локатор для выпадающего списка с вопросами
     private By voprosiOvajnom = By.xpath("//div[@class = 'accordion__button']");
     // Локатор для текста из выпадающего списка
     private By voprosiOvajnomText = By.xpath("//div[@class = 'accordion__panel']/p");
     // Кукиии!!!
-    private By ubratCookie = By.xpath("//button[@id = 'rcc-confirm-button']");
+    private By buttonCookie = By.xpath("//button[@id = 'rcc-confirm-button']");
 
     // Локатор для кнопки заказать вверху справа
     private By buttonZakazatHigh = By.xpath("//button[@class = 'Button_Button__ra12g']");
@@ -33,10 +49,35 @@ public class StartPage {
     private By buttonZakazatLow = By.xpath("//div[@class = 'Home_FinishButton__1_cWm']/button");
 
 
+
     public StartPage (WebDriver driver){
         this.driver = driver;
         this.wait = new WebDriverWait(driver, Duration.ofSeconds(3));
     }
+
+
+    //Скролим до листа с вопросами
+    public void listVoprosiScroll () {
+        WebElement element = driver.findElement(listVoprosi);
+        ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView(true);", element);
+    }
+
+    // Создаем и наполняем список
+    public List <WebElement> getListButtons () {
+        return driver.findElements(voprosiOvajnom);
+    }
+
+    // Кликнуть на вопрос по индексу
+    public void clickListButtons (int index) {
+        getListButtons().get(index).click();
+    }
+
+    // Заполняем лист с ответами
+    public List<WebElement> getAnswerTextList(){
+        return  driver.findElements(voprosiOvajnomText);
+    }
+
+
 
 
     //Кликаем на кнопку Заказать вверху справа
@@ -51,24 +92,10 @@ public class StartPage {
 
     // Кликаем на куки
     public void clickButtonCookie (){
-        driver.findElement(ubratCookie).click();
-    }
-
-    // Создаем и наполняем список
-    public List <WebElement> getListButtons () {
-        return driver.findElements(voprosiOvajnom);
+        driver.findElement(buttonCookie).click();
     }
 
 
-    // Заполняем лист с ответами
-    public List<WebElement> getAnswerTextList(){
-        return  driver.findElements(voprosiOvajnomText);
-    }
-
-    // Кликнуть на вопрос по индексу
-    public void clickListButtons (int index) {
-        getListButtons().get(index).click();
-    }
 
     // Спарсить текст ответа
     public String parsingAnswerList (int index) {
@@ -77,5 +104,19 @@ public class StartPage {
         wait.until(ExpectedConditions.visibilityOf(otvet));
             return otvet.getText();
         }
+
+
+    //Метод через верхнюю кнопку Заказать
+    public void highButtonZakazat () {
+        clickButtonCookie();
+        clickButtonZazatHigh();
+
+    }
+
+
+    public void lowButtonZakazat () {
+        clickButtonCookie();
+        clickButtonZakazatLow();
+    }
 
 }
